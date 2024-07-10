dac1153835693973c126176959d222e0d7f33b9a
Leon-Leyang <leyang_hu@brown.edu>
2024-07-10 14:39:30 -0400

    Change slurm config

--- myproj/grids/lr.py
+++ myproj/grids/lr.py
@@ -5,12 +5,10 @@ from dora import Explorer, Launcher
 def explorer(launcher: Launcher):
     launcher.slurm_(
         gpus=2,
-        mem_per_gpu=32000,
         partition="gpu",
-        time="12:00:00",
-        job_name="lr-grid",
-        mail_type='ALL',
-        mail_user='leyang_hu@brown.edu'
+        time="720",
+        cpus_per_gpu=2,
+        setup=['module load miniconda3/23.11.0s', 'conda activate bs'],
     )
 
     # Example iterating over multiple learning rates and batch sizes
