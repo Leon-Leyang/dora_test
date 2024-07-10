55a7b20affae8c7d235b783ff7d6e55811aa1e35
Leon-Leyang <leyang_hu@brown.edu>
2024-07-10 13:40:51 -0400

    Add slurm specification

--- myproj/grids/lr.py
+++ myproj/grids/lr.py
@@ -3,6 +3,21 @@ from dora import Explorer, Launcher
 
 @Explorer
 def experiment(launcher: Launcher):
+    launcher.slurm_(
+        gpus=2,
+        mem_per_gpu=32000,
+        partition="gpu",
+        time="12:00:00",
+        job_name="lr-grid",
+        mail_type='ALL',
+        mail_user='leyang_hu@brown.edu'
+    )
+
     # Example iterating over multiple learning rates and batch sizes
     for lr in [0.001, 0.01, 0.1]:
-        launcher(f'optimizer.lr={lr}')
+        sub = launcher.bind([f'optimizer.lr={lr}'])
+        sub.slurm_(
+            output=f"logs/lr-{lr}.out",
+            error=f"logs/lr-{lr}.out"
+        )
+        sub()
